dbffd31c19894c24266f810919b4d3cc9b4bfad2
Ilias Shaikh <iliasashaikh@gmail.com>
2010-02-15 16:09:14 +0000

    added console host

---
+++ ServiceHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+
+namespace ConsoleServiceHost
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ServiceHost host = new ServiceHost(typeof(OrderService.Orders));
+      host.Open();
+      Console.WriteLine("Any key to stop");
+      Console.ReadKey();
+    }
+  }
+}
